486c5a1a7464963d5bcdd73255589efddf56c8c8
ClearKillah <fillop330@gmail.com>
2025-03-03 01:15:58 +0400

    123

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = (env, argv) => {
     entry: './src/index.tsx',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
+      publicPath: '/'
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js']
@@ -23,7 +24,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+          use: ['style-loader', 'css-loader', 'postcss-loader']
         }
       ]
     },
@@ -37,7 +38,8 @@ module.exports = (env, argv) => {
     ],
     devServer: {
       port: 3000,
-      hot: true
+      hot: true,
+      historyApiFallback: true
     }
   };
 }; 
